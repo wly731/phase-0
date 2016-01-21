de0e076a6a3ba8a017258eb5bb5fb6947d9c5aa0
wly731 <wly731@gmail.com>
2016-01-22 00:40:34 +0900

    complete 4.3.1

---
+++ week-4/address/my_solution.rb
@@ -0,0 +1,13 @@
+# Format an Address
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def make_address (street, city, state, zip)
+  puts "You live at " + street + ", in the beautiful city of " + city + ", " + state + ". Your zip is " + zip.to_s + "."
+
+end
+
+make_address("633 Folsom St.", "San Francisco", "CA", "94107")
---
+++ week-4/define-method/my_solution.rb
@@ -0,0 +1,11 @@
+
+# Define an Empty Method
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def do_something(p1,p2,p3)
+
+end
