0a9531d5a1011a22281288a6b92a5efc4c119fc4
wly731 <wly731@gmail.com>
2016-02-01 12:48:25 +0800

    complete reflection

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -74,5 +74,19 @@ def mode(mode_arr)
 end
 
 
-
 # 4. Reflection
+=begin
+
+Which data structure did you and your pair decide to implement and why?
+Ans: We decided to use hash because we wanted to assign the values from mode_arr as the keys and the frequency as the hash values.
+
+Were you more successful breaking this problem down into implementable pseudocode than the last with a pair?
+Ans: Yes, we had no problem in dicussing the pseudocode together as a pair
+
+What issues/successes did you run into when translating your pseudocode to code?
+Ans: Sometimes we just did not know how to translate our idea in our pseudocode into actual codes.
+
+What methods did you use to iterate through the content? Did you find any good ones when you were refactoring? Were they difficult to implement?
+Ans: We used .each and .select to iterate through the content of the hash. They were difficult to understand in the beginning, but we managed to use .select in our solution.
+
+=end
