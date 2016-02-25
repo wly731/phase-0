1e9faee79a0354794c0cef97652d83467c0ee893
wly731 <wly731@gmail.com>
2016-02-24 19:02:18 -0800

    complete 9.3.1 with reflection

--- week-9/home_page.js
+++ week-9/home_page.js
@@ -1,10 +1,35 @@
+// DOM Manipulation Challenge
+
+
+// I worked on this challenge with: Brett Ripley
+
+
+// Add your JavaScript calls to this page:
+
+// Release 0:
+
 document.getElementById("release-0").className = "done";
+
+
+// Release 1:
 
 document.getElementById("release-1").style.display = "none";
 
+
+
+
+// Release 2:
+
 document.getElementsByTagName('h1')[0].textContent = "I completed release 2";
 
+
+// Release 3:
+
 document.getElementById("release-3").style.backgroundColor = "#955251" ;
+
+
+
+// Release 4:
 
 var i;
 for (i=0; i < document.getElementsByClassName("release-4").length; i++){
@@ -12,7 +37,29 @@ for (i=0; i < document.getElementsByClassName("release-4").length; i++){
 
 }
 
+// Release 5:
+
 var tmpl = document.getElementById('hidden');
 document.body.appendChild(tmpl.content.cloneNode(true));
 
 
+
+// Reflection:
+
+// What did you learn about the DOM?
+
+// Ans: Before we learned about the general structure of DOM. Now I feel more concrete since I learned how to access actually change a DOM node using JavaScript.
+
+
+// What are some useful methods to use to manipulate the DOM?
+
+// Ans: I always use
+// document.getElementById
+// document.getElementsByClassName
+// docuemtn.getElementsByTagName
+
+// and then followed by
+
+// .style
+
+// to change some attributes of that element.
